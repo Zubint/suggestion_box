83a2ede3cf5c23ba01b23149dde4378b10b1b6b1
Zubint <zubintalib@izooky.com>
2016-01-22 13:38:53 -0600

    Latest Sync

--- js/controllers/SuggestionController.js
+++ js/controllers/SuggestionController.js
@@ -1,5 +1,7 @@
 app.controller('SuggestionController', ['$scope', '$routeParams', 'suggestions', function($scope, $routeParams, suggestions) {
 
+		
+
 		$scope.post = suggestions.posts[$routeParams.id];
 
 
